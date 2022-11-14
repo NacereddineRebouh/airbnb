9b8d9c9b9ba41f6cbe497c0ed2248bd884285b1b
Nacereddine_rbh <llbl2153@gmail.com>
2022-11-14 10:29:45 +0100

    installing ui library for meny

---
+++ components/Header.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Image from "next/image";
+import {
+  MagnifyingGlassCircleIcon,
+  UserCircleIcon,
+  Bars3Icon,
+  ChevronDownIcon,
+} from "@heroicons/react/24/solid";
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <div className=" bg-white">
+      {/* Desktop and tablets */}
+      <div className="border-b-[1px]">
+        <div className="grid grid-cols-[30px_40px_minmax(0px,_360px)_minmax(265px,_2fr)_30px] md:grid-cols-[30px_2fr_350px_minmax(265px,_2fr)_30px] lg:grid-cols-[70px_2fr_350px_minmax(265px,_2fr)_70px] gap-0 collapse mobile:visible h-0 mobile:h-20 self-center max-w-[2500px] mx-auto">
+          <div className="relative col-start-2 flex items-center justify-left md:mt-1 md:p-2 mr-2">
+            <Image
+              className="cursor-pointer collapse md:visible"
+              src={"/images/airbnb2.png"}
+              alt={""}
+              width={100}
+              height={50}
+            />
+            <Image
+              className="absolute cursor-pointer w-20 collapse mobile:visible md:collapse"
+              src={"/images/airbnb_small.png"}
+              alt={""}
+              width={100}
+              height={50}
+            />
+          </div>
+          <div className="relative col-start-3 flex items-center justify-start md:justify-center  md:mx-0 font-semibold">
+            <div className="flex flex-row w-[350px] h-12 border-[1.5px] rounded-full shadow-md transition-shadow duration-300 hover:shadow-lg hover:drop-shadow-sm items-center justify-start text-[15px] ">
+              <div className="flex border-r-[1.2px] px-4 pl-5 cursor-pointer truncate">
+                Anywhere
+              </div>
+              <div className="flex border-r-[1.2px] px-4 cursor-pointer truncate">
+                Any week
+              </div>
+              <div className="flex flex-row justify-center items-center pl-4 text-gray-400 font-normal cursor-pointer gap-2">
+                <span className="truncate">Add guests</span>
+                <MagnifyingGlassCircleIcon className="rounded-full text-[#ff385c] font-bold cursor-pointer h-9 ml-1" />
+              </div>
+            </div>
+          </div>
+          <div className="relative col-start-4 flex flex-row gap-4 items-center justify-end mx-3 font-semibold ">
+            <div className="flex flex-row items-center justify-center gap-4">
+              <div className=" rounded-full transition-colors duration-200 hover:bg-gray-100 flex items-center font-semibold cursor-pointer h-11 px-2 text-[14px] truncate">
+                Become a Host
+              </div>
+              <div className="transition-colors duration-200 h-8 w-8 hover:bg-gray-100 rounded-full flex items-center justify-center">
+                <GlobeAltIcon className="rounded-full  font-bold cursor-pointer h-4" />
+              </div>
+            </div>
+            <div
+              className="h-11 border-[1.5px] rounded-full flex items-center justify-center w-[79px] cursor-pointer gap-1 transition-shadow duration-300 hover:shadow-md "
+              onClick={() => {
+                //open menu
+              }}
+            >
+              <Bars3Icon className="rounded-full text-gray-700 font-bold cursor-pointer h-6" />
+              <UserCircleIcon className="rounded-full text-gray-500 font-bold cursor-pointer h-9" />
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Search Bar */}
+        <div className="visible mobile:collapse h-20 mobile:h-0 flex items-end justify-center font-semibold">
+          Mobile Search Bar
+        </div>
+      </div>
+    </div>
+  );
+}
